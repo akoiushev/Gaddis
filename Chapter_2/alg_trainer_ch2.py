93f0b8a7f399b25baaacb50c638d8231526ae275
akoiushev <62853401+akoiushev@users.noreply.github.com>
2022-09-10 17:38:42 +0300

    Выполнены упражнения алгоритмического тренажера с 5 по 11 , глава 2.

---
+++ Chapter_2/alg_trainer_ch2.py
@@ -0,0 +1,25 @@
+# 5
+total = 10 + 14
+print(total)
+print("-" * 10)
+
+# 6
+down_payment = 5
+due = total - down_payment
+print(f"К оплате: {due} р.")
+print("-" * 10)
+
+# 7
+subtotal = 100
+total = subtotal * 0.15
+print(f"Нарастающий итог: {total} р.")
+print("-" * 10)
+
+# 10
+sales = 100.123456789
+print(round(sales, 2))
+print("-" * 10)
+
+# 11
+number = 1234567.456
+print(format(number, ',.1f'))
