683e3d685b876b31dd0df21fa6ab3fabb0203dbb
cognizer <62853401+akoiushev@users.noreply.github.com>
2024-11-07 01:48:57 +0300

    Создана папка по Главу 10. Классы.

--- Chapter_2/orion.py
+++ Chapter_2/orion.py
@@ -1,15 +1,16 @@
-# Эта программа наносит звезды созвесздия Ориона,
-# названия звезд и линии создвездия.
-import turtle
+#Эта программа наносит звезды созвесздия Ориона,
+#названия звезд и линии создвездия.
+import		turtle
 
-# Задать размер окна
-turtle.setup(500, 600)
+#Задать размер окна
+		turtle.
+setup(500, 600)
 
-# Установить черепаху
+#Установить черепаху
 turtle.penup()
 turtle.hideturtle()
 
-# Создать именованные константы для звездных координат.
+#Создать именованные константы для звездных координат.
 LEFT_SHOULDER_X = -70
 LEFT_SHOULDER_Y = 200
 
@@ -31,72 +32,86 @@ LEFT_KNEE_Y = -180
 RIGHT_KNEE_X = 120
 RIGHT_KNEE_Y = -140
 
-# Нанести звезды
-turtle.goto(LEFT_SHOULDER_X, LEFT_SHOULDER_Y) # Левое плечо
+#Нанести звезды
+turtle.goto (LEFT_SHOULDER_X, LEFT_SHOULDER_Y)
+#Левое плечо
 turtle.dot()
-turtle.goto(RIGHT_SHOULDER_X, RIGHT_SHOULDER_Y) # Правое плечо
+turtle.goto (RIGHT_SHOULDER_X, RIGHT_SHOULDER_Y)
+#Правое плечо
 turtle.dot()
-turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)# Левая звезда в поясе
+turtle.goto (LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+#Левая звезда в поясе
 turtle.dot()
-turtle.goto(MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y) # Средняя звезда
+turtle.goto (MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y)
+#Средняя звезда
 turtle.dot()
-turtle.goto(RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y) # Правая звезда
+turtle.goto (RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
+#Правая звезда
 turtle.dot()
-turtle.goto(LEFT_KNEE_X, LEFT_KNEE_Y) # Левое колено
+turtle.goto (LEFT_KNEE_X, LEFT_KNEE_Y)
+#Левое колено
 turtle.dot()
-turtle.goto(RIGHT_KNEE_X, RIGHT_KNEE_Y) # Правое колено
+turtle.goto (RIGHT_KNEE_X, RIGHT_KNEE_Y)
+#Правое колено
 turtle.dot()
 
-# Вывести названия звезд
-turtle.goto(LEFT_SHOULDER_X,LEFT_SHOULDER_Y) # Левое плечо
+#Вывести названия звезд
+turtle.goto (LEFT_SHOULDER_X, LEFT_SHOULDER_Y)
+#Левое плечо
 turtle.write("Бетельгейзе")
-turtle.goto(RIGHT_SHOULDER_X, RIGHT_SHOULDER_Y) # Правое плечо
+turtle.goto (RIGHT_SHOULDER_X, RIGHT_SHOULDER_Y)
+#Правое плечо
 turtle.write("Хатиса")
-turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y) # Левая звезда в поясе
+turtle.goto (LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+#Левая звезда в поясе
 turtle.write("Альнитак")
-turtle.goto(MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y) # Средняя звезда
+turtle.goto (MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y)
+#Средняя звезда
 turtle.write("Альнилам")
-turtle.goto(RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)# Правая звезда
+turtle.goto (RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
+#Правая звезда
 turtle.write("Минтака")
-turtle.goto(LEFT_KNEE_X,LEFT_KNEE_Y) # Левое колено
+turtle.goto (LEFT_KNEE_X, LEFT_KNEE_Y)
+#Левое колено
 turtle.write("Саиф")
-turtle.goto(RIGHT_KNEE_X, RIGHT_KNEE_Y) # Правое колено
+turtle.goto (RIGHT_KNEE_X, RIGHT_KNEE_Y)
+#Правое колено
 turtle.write("Ригель")
 
-# Нанести линию из левого плеча в левую звезду пояса
-turtle.goto(LEFT_SHOULDER_X, LEFT_SHOULDER_Y)
+#Нанести линию из левого плеча в левую звезду пояса
+turtle.goto (LEFT_SHOULDER_X, LEFT_SHOULDER_Y)
 turtle.pendown()
-turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+turtle.goto (LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
 turtle.penup()
 
-# Нанести линию из правого плеча в правую звезду пояса
-turtle.goto(RIGHT_SHOULDER_X, RIGHT_SHOULDER_Y)
+#Нанести линию из правого плеча в правую звезду пояса
+turtle.goto (RIGHT_SHOULDER_X, RIGHT_SHOULDER_Y)
 turtle.pendown()
-turtle.goto(RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
+turtle.goto (RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
 turtle.penup()
 
-# Нанести линию из левой звезды пояса в среднюю звезду пояса
-turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+#Нанести линию из левой звезды пояса в среднюю звезду пояса
+turtle.goto (LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
 turtle.pendown()
-turtle.goto(MIDDLE_BELTSTAR_X,MIDDLE_BELTSTAR_Y)
+turtle.goto (MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y)
 turtle.penup()
 
-# Нанести линию из средней звезды пояса в правую звезду пояса
-turtle.goto(MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y)
+#Нанести линию из средней звезды пояса в правую звезду пояса
+turtle.goto (MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y)
 turtle.pendown()
-turtle.goto(RIGHT_BELTSTAR_X,RIGHT_BELTSTAR_Y)
+turtle.goto (RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
 turtle.penup()
 
-# Нанести из левой звезды пояса в левое колено
-turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+#Нанести из левой звезды пояса в левое колено
+turtle.goto (LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
 turtle.pendown()
-turtle.goto(LEFT_KNEE_X, LEFT_KNEE_Y)
+turtle.goto (LEFT_KNEE_X, LEFT_KNEE_Y)
 turtle.penup()
 
-# Нанести линию из правой звезды пояса в правое колено
-turtle.goto(RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
+#Нанести линию из правой звезды пояса в правое колено
+turtle.goto (RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
 turtle.pendown()
-turtle.goto(RIGHT_KNEE_X, RIGHT_KNEE_Y)
+turtle.goto (RIGHT_KNEE_X, RIGHT_KNEE_Y)
 
-# Оставить окно открытым
+#Оставить окно открытым
 turtle.done()
